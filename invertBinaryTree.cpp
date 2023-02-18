54af7be03f368862be27ffa91ae7f3efb6ff0df8
hemchand <hemuchandravanshi03@gmail.com>
2023-02-18 23:59:13 +0530

    ..

---
+++ invertBinaryTree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void preorder(TreeNode *node)
+    {
+        if(node == NULL) return;
+        else if(node->left == NULL && node->right == NULL) return;
+        else if(node->left != NULL && node->right != NULL)
+        {
+            TreeNode *temp = node->left;
+            node->left = node->right;
+            node->right = temp;
+        }
+        else if(node->left == NULL)
+        {
+            node->left = node->right;
+            node->right = NULL;
+        }
+        else if(node->right == NULL)
+        {
+            node->right = node->left;
+            node->left = NULL;
+        }
+        
+        preorder(node->left);
+        preorder(node->right);
+    }
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL) return NULL;
+        if(root ->left == NULL && root->right == NULL )return root;
+        preorder(root);
+        return root;
+    }
+};

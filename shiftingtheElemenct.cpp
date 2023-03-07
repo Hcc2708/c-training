1d9ae94ea5ddda69748cd9aea7a6ec0f84e17efd
hemchand <hemuchandravanshi03@gmail.com>
2023-03-07 23:43:03 +0530

    ..

---
+++ shiftingtheElemenct.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[] = {3,4,5,6,7,9};
+    int arr1[6];
+    int r = 2;
+    int j = 0
+    for(int i=0; i<6; i++)
+    {
+    if(j+r != 6)
+    {arr1[j+2] = arr[i];}
+
+    if(j+r > 6)
+    {
+        arr1[n-1-j] = arr[]
+    }
+    j+=2;
+
+    }
+
+}

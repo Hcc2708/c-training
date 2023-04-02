19f0468e076f4675ba2ddace1b99667c0c0bca2a
hemchand <hemuchandravanshi03@gmail.com>
2023-04-02 23:54:14 +0530

    ..

---
+++ variable.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello world";
+    printf("%d", 5);
+    //int - 5
+    //double -5.55
+    //char -'a' or 'A'
+    //string - "Hello World"
+    //bool - true/false
+    const int a = 5; // constant variable
+    double b = 5.5;
+    float c = 3.3;
+    cout<<c;
+
+    double d;
+    cout<<"Enter the number";
+    cin>>d;
+    cout<<d;
+
+    //string x  = 'abc'; doesn't work;
+    cout<<x;
+    string s;
+    cout<<endl;
+    cin>>s;
+    cout<<s;
+}

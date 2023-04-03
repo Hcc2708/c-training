f91069bab0f12055f360723c95d4ae5f25c2da35
hemchand <hemuchandravanshi03@gmail.com>
2023-04-04 00:07:51 +0530

    ..

--- boatsToSavePeople.cpp
+++ boatsToSavePeople.cpp
@@ -4,18 +4,27 @@ public:
         sort(people.begin(), people.end());
         int ans = 0;
         int sum = 0;
-        for(int i = 0; i< people.size(); i++)
+        int j = people.size()-1;
+        int i = 0;
+        while(i <= j)
         {
-            
-            sum += people[i];
+            if(i != j)
+            sum = people[i] + people[j];
+            else sum = people[i];
             if(sum > limit)
             {
                 ans++;
-                sum = people[i];
+                sum = 0;
+                j--;
             }
-
+            else
+                {
+                    ans++;
+                    i++; 
+                    j--;
+                    sum = 0;
+                }
         }
-        ans++;
         return ans;
     }
 };
